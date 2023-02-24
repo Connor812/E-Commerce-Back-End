181c37cc7ab1892ac0cf6679bb71890c84880846
Connor Savoy <connorsavoy812@gmail.com>
2023-02-24 09:59:23 -0500

    added the start code. And i got all the models set up and seeded my data.

---
+++ models/index.js
@@ -0,0 +1,44 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: 'ProductTag',
+    as: 'product_tags'
+  },
+});
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+Category.hasMany(Tag, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    as: 'product_tags',
+  },
+});
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
